77a52c95f130e71358963552d6817bfc3ccefc90
Jock Dalby <jock.dalby@citrusad.com>
2022-06-03 10:59:53 +1000

    add setProductActiveProp to ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, firstValueFrom, Observable, of } from 'rxjs';
 
 export interface IProduct {
   id: string;
@@ -54,14 +54,30 @@ export const DUMMY_PRODUCTS: IProduct[] = [
 })
 export class ProductService {
 
+  dummyProductsBS = new BehaviorSubject<IProduct[]>(DUMMY_PRODUCTS);
+
   getProductById(id: string): Observable<IProduct | undefined> {
-    const product = DUMMY_PRODUCTS.find(p => p.id === id);
+    const product = this.dummyProductsBS.value.find(p => p.id === id);
     return of(product);
   }
 
   getProductsOfSameCategory(productCode: number): Observable<IProduct[]> {
-    const product = DUMMY_PRODUCTS.find((p) => p.productCode === productCode);
+    const product = this.dummyProductsBS.value.find((p) => p.productCode === productCode);
     const similarProducts = DUMMY_PRODUCTS.filter(p => p.category === product?.category)
     return of(similarProducts);
+  }
+
+  setProductActiveProp(id: string, active: boolean): void {
+    const products = this.dummyProductsBS.value;
+    const updatedProducts = products.map(p => {
+      if(p.id === id) {
+        return {
+          ...p,
+          active
+        }
+      }
+      return p
+    })
+    this.dummyProductsBS.next(updatedProducts)
   }
 }
